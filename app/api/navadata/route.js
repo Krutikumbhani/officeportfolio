8aca02f6f9f85c6f5138d6d920ecceb2f7842fa1
Krutikumbhani <krutikumbhani396@gmail.com>
2025-04-01 17:05:22 +0530

    edit in route page

--- app/api/navadata/route.js
+++ app/api/navadata/route.js
@@ -1,37 +1,3 @@
-// export const navdata = [
-//   {
-//     id: '1',
-//     name: 'Home',
-//   },
-//   {
-//     id: '2',
-//     name: 'Services',
-//   },
-//   {
-//     id: '3',
-//     name: 'Resume',
-//   },
-//   ,
-//   {
-//     id: '4',
-//     name: 'Mywork',
-//   },
-//   {
-//     id: '5',
-//     name: 'Contact',
-//   },
-// ];
-
-
-// export async function GET() {
-//   return Response.json([
-//     { id: "1", name: "Home" },
-//     { id: "2", name: "Services" },
-//     { id: "3", name: "Resume" },
-//     { id: "4", name: "Mywork" },
-//     { id: "5", name: "Contact" }
-//   ]);
-// }
 
 export async function GET() {
   return new Response(JSON.stringify([
